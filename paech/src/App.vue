672d14be63021a3ddac1a44e30abc72aa4690a61
Administrator <Administrator@contoso2019.local>
2020-04-20 17:28:51 +0800

    第一次提交

---
+++ paech/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app"> 
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>         
+        <router-view/>
+      </el-main>
+    </el-container>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="less">
+  @import url(./assets/css/common.less);
+  @import url(./assets/css/header.less);
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ paech/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="home">
+        商城首页
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data () {
+            
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    @import url(../assets/css/home.less);
+</style>
